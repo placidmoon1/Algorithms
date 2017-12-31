c9bc6753a40f1d3b7f99993da03b6547525eadbd
Kevin Min <placidmoon1@gmail.com>
2018-01-01 01:44:10 +0900

    Update and rename Reverse-String.js to Reverse A String.js

---
+++ Basic/Basic Algorithms Scripting/Reverse A String.js
@@ -0,0 +1,22 @@
+/* 
+FreeCodeCamp Basic Algorithms Scripting Q1: <Reverse a String>
+Reverse the provided string.
+You may need to turn the string into an array before you can reverse it.
+Your result must be a string.
+*/
+
+/* Reversing a sting using an iterative approach */
+function reverseStringForLoop(str) {
+  newStr = "";
+  for (var i = str.length - 1 ; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+}
+
+/* Reversing a string using javaScript functions*/
+function reverseStringJSFunc(str) {
+  return str.split(' ').reverse().join('');
+}
+reverseStringForLoop("hello");
+reverseStringJSFunc("hello");
