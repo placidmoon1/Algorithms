f3d2ba53089c05638beba6414ef93902b3f4c809
Kevin Min <placidmoon1@gmail.com>
2018-01-01 04:47:13 +0900

    Update and rename titleCase.js to 5. Title Case a String.js

--- Basic/Basic Algorithms Scripting/5. Title Case a String.js
+++ Basic/Basic Algorithms Scripting/5. Title Case a String.js
@@ -1,22 +1,20 @@
-/* FreeCodeCamp Basic Algorithms Scripting Q5: Tile Case a Sentence
+/* FreeCodeCamp Basic Algorithms Scripting Q5: <Tile Case a Sentence>
 
 Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
-
 For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
-
 */
 
-function titleCase(str) {
+/* Iterative approach */
+function titleCaseIter(str) {
   var strArr = str.split(" ");
   for (var i = 0; i < strArr.length; i++) {
-    var subStrArr = strArr[i].split("");
-    subStrArr[0] = subStrArr[0].toUpperCase();
-    for (var j = 1; j < subStrArr.length; j++) {
-      subStrArr[j] = subStrArr[j].toLowerCase();
-    }
-    strArr[i] = subStrArr.join("");
+    strArr[i] = strArr[i].toLowerCase().split("");
+    strArr[i][0] = strArr[i][0].toUpperCase();
+    strArr[i] = strArr[i].join("");
   }
   return strArr.join(" ");
 }
+
+titleCaseIter("I'm a little tea pot");
 
 titleCase("I'm a little tea pot");
