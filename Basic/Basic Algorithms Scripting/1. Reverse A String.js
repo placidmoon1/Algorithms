2280c3bb7201d99a1f47e05ae0485500cb3040db
Kevin Min <placidmoon1@gmail.com>
2018-01-01 02:20:57 +0900

    Update 1. Reverse A String.js

--- Basic/Basic Algorithms Scripting/1. Reverse A String.js
+++ Basic/Basic Algorithms Scripting/1. Reverse A String.js
@@ -1,5 +1,6 @@
 /* 
 FreeCodeCamp Basic Algorithms Scripting Q1: <Reverse a String>
+
 Reverse the provided string.
 You may need to turn the string into an array before you can reverse it.
 Your result must be a string.
