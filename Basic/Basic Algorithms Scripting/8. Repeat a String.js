da758b852039d04c7d993357afa13f5662f9ef2e
Kevin Min <placidmoon1@gmail.com>
2018-01-01 07:49:36 +0900

    Create 8. Repeat a String.js

---
+++ Basic/Basic Algorithms Scripting/8. Repeat a String.js
@@ -0,0 +1,29 @@
+/*
+FreeCodeCamp Basic Algorithms Scription Q8: <Repeat a String>
+
+Repeat a given string (first argument) num times (second argument). 
+Return an empty string if num is not a positive number.
+*/
+
+/* Iterative Approach */
+function repeatStringNumTimesIter(str, num) {
+  if (num < 0) {
+    return "";
+  }
+  var resultString = "";
+ for (var i = 0; i < num; i++) {
+   resultString += str;
+ }
+  return resultString;
+}
+/* Recusrive Approach */
+function repeatStringNumTimesRecur(str, num) {
+  if (num <= 0) {
+    return "";
+  }
+  return str += repeatStringNumTimes(str, num - 1);
+}
+
+/* Test Cases; all test cases should return "abcabcabc"*/
+repeatStringNumTimesRecur("abc", 3);
+repeatStringNumTimesIter("abc", 3);
