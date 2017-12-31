43a61ecbe9384f9a5e5d69cc2d8a6c69202d4b86
Kevin Min <placidmoon1@gmail.com>
2018-01-01 02:21:14 +0900

    Update 2. Factorialize.js

--- Basic/Basic Algorithms Scripting/2. Factorialize.js
+++ Basic/Basic Algorithms Scripting/2. Factorialize.js
@@ -1,5 +1,6 @@
 /* 
 Basic Algorithm Scripting FreeCodeCamp Q2: <Factorialize a Number>
+
 Return the factorial of the provided integer.
 If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
 Factorials are often represented with the shorthand notation n!
