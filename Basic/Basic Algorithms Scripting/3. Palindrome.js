52dd885b0c7d65d5d846bcc95d2f0dec81e40bc5
Kevin Min <placidmoon1@gmail.com>
2018-01-01 02:20:43 +0900

    Update 3. Palindrome.js

--- Basic/Basic Algorithms Scripting/3. Palindrome.js
+++ Basic/Basic Algorithms Scripting/3. Palindrome.js
@@ -1,9 +1,9 @@
 /* FreeCodeCamp Basic Alhorithms Scripting Q3: Check for Palindromes
-Return true if the given string is a palindrome. Otherwise, return false.
 
+Return true if the given string is a palindrome. Otherwise, return false.
 A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
 
-Note
+Notes:
 You'll need to remove all non-alphanumeric characters and turn everything lower case in order to check for palindromes.
 We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
 We'll also pass strings with special symbols, such as "2A3*3a2", "2A3 3a2", and "2_A3*3#A2".
@@ -47,4 +47,4 @@ function palindromeDnD(str) {
 }
 
 var ans = palindromeDnD("eye"); //should return true
-var ans = palindromeDnD("hello); //should return false
+var ans = palindromeDnD("hello"); //should return false
