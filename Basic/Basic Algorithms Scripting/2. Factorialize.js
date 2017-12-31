346f050ed907dd04aeb11ea65792dfc7b1d1bf0b
Kevin Min <placidmoon1@gmail.com>
2018-01-01 01:50:29 +0900

    Update and rename Factorialize.js to 2. Factorialize.js

--- Basic/Basic Algorithms Scripting/2. Factorialize.js
+++ Basic/Basic Algorithms Scripting/2. Factorialize.js
@@ -7,13 +7,24 @@ Factorials are often represented with the shorthand notation n!
 For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
 */
 
-// Parameter specification: Parameter has to be a non-negative integer
+/* Parameter specification: Parameter has to be a non-negative integer */
+/* Iterative Approach*/
+function factorializeIter(num) {
+  var iterSol = 1;
+  for (var i = 1; i <= num; i++) {
+    iterSol *= i;
+  }
+  return iterSol; // if num == 0, function would output 1 per initialization specification of var iterSol!
+}
+
 /* Recursive Approach */
-function factorialize(num) {
+function factorializeRecur(num) {
   if (num == 1 || num == 0) {
     return 1;
   }
   return num * factorialize(num-1);
 }
 
-factorialize(5);
+/* Sample test should output 120 (1*2*3*4*5) */
+factorializeIter(5);
+factorializeRecur(5);
